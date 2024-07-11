3251abf1aee1016319550ca0ebe657b1894e829d
nag0yan <matsui-ka@kadokawa.jp>
2024-07-11 23:00:11 +0900

    Initial commit

---
+++ test/basetion.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Basetion from '../lib/basetion-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/basetion-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Basetion.BasetionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
